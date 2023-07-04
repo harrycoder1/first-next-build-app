3b3286eda8a6331a105638891f6a84ec16886f26
harycoder1 <harish7823@gmail.com>
2023-07-04 23:21:24 +0530

    initial commit for the deployement

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  // experimental: {
+  //   esmExternals: "loose",
+  //   serverComponentsExternalPackages: ["mongoose"] 
+  // },
+  // // and the following to enable top-level await support for Webpack
+  // webpack: (config) => {
+  //   config.experiments = {
+  //     topLevelAwait: true
+  //   };
+  //   return config;
+  // },
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'occ-0-3933-116.1.nflxso.net',
+          port: '',
+          pathname: '**',
+        },
+      ],
+    },
+
+    
+  }
+
+// module.exports = nextConfig
